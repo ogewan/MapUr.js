10a0781110719c918ca40f2ab8d89506431470e4
ogewan <seun40@gmail.com>
2016-04-12 20:48:30 -0500

    drawing global objects

--- test.js
+++ test.js
@@ -1,19 +1,83 @@
 !(function(){
+    //μ = {clone: };
+    //μ.base = μ.clone(window);
     var test = function(){
-        console.log("My World");
-        var p = document.createElement("p");
-        var o = document.createElement("p");
-        o.id = "testingID";
-        p.innerHTML = "My World";
-        o.innerHTML = 0;
+        var simpledeep = function(obj){
+            var cache = [];
+            var res = JSON.stringify(obj, function(key, value) {
+                if (typeof value === 'object' && value !== null){
+                    if (cache.indexOf(value) !== -1) {
+                        return;
+                    }
+                    cache.push(value);
+                }
+                return value;
+            });
+            cache = null;
+            return JSON.parse(res);
+        }
         
-        document.body.appendChild(p);
-        document.body.appendChild(o);
-        //window.setInterval(function(document.getElementById("testingID").innerHTML=parseInt(document.getElementById("testingID").innerHTML)+1), 1000);
-        window.setInterval(
-            function(){
-                document.getElementById("testingID").innerHTML = parseInt(document.getElementById("testingID").innerHTML) + 1
-            }, 1000);
+        mU = {clone: simpledeep, base: simpledeep(window), poll: null, cycles: 0, pause: function(){mU.remote=0}, start: function(){mU.remote=1;mU.poll()}, remote: 1, garage: {}};
+        mU.sidereal = function(par,obj){
+            var mobj = {};
+            var active = simpledeep(obj);
+            for(var item in active){
+                if(par[item]===void 0){
+                    mobj[item] = active[item];
+                } else if (typeof mU.base[item]==="object"){
+                    mobj[item] = mU.sidereal(active[item]);
+                }
+            }
+            return mobj;
+        }
+        mU.poll = function(){
+            mU.cycles++;
+            mU.garage = {};
+            var active = simpledeep(window);
+            for(var item in active){
+                if(mU.base[item]===void 0){
+                    mU.garage[item] = active[item];
+                } else if (typeof mU.base[item]==="object"){
+                    //mU.garage[item] = mU.sidereal(mU.base[item],active[item])
+                }
+            }
+            mU.draw();
+            if(mU.remote) return setTimeout(mU.poll,500);
+        }
+        mU.draw = function(){
+            if(mU.canvas===void 0){
+                try {
+                    mU.canvas = document.createElement('canvas');
+                    mU.canvas.width = window.innerWidth;
+                    mU.canvas.height = window.innerHeight;
+                    document.body.appendChild(mU.canvas);
+                    mU.ctx = mU.canvas.getContext('2d');
+                }
+                catch(err) {
+                    mU.canvas = void 0;
+                    return setTimeout(mU.draw,100);
+                }
+            }
+            //resize canvas
+            mU.canvas.width = window.innerWidth;
+            mU.canvas.height = window.innerHeight;
+            //draw objects in garage
+            var cnt, cur=0;
+            for(var i in mU.garage){
+                if(mU.garage.hasOwnProperty(i)) {
+                    cnt++;
+                }
+            }
+            for(var item in mU.garage){
+                if(mU.garage.hasOwnProperty(item)) {
+                    //mU.garage[item];
+                    mU.ctx.fillRect(50,50*cur,25,25);
+                    cur++;
+                }
+            }
+        }
+        mU.poll();
+        FLAG = "NEW WINDOW VARIABLE!";
     }
     try {
         test();
